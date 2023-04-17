d8fa93ecae781f8acf86846ca9c219f74e25fde8
haval95 <havalrezan22@gmail.com>
2023-04-17 23:39:26 +0300

    FEAT: comparing frames DESCRIPTION: - we capture the first frame then compare it with the live camera, and we take the difference between the current frame and the first one
    - we make the img blury so it would cancel some noises

--- motion_detect.py
+++ motion_detect.py
@@ -2,13 +2,24 @@ import cv2, time
 
 
 video = cv2.VideoCapture(0)
-
+first_frame = None
 
 while True:
     check, frame = video.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-           
-    cv2.imshow("live", gray)
+    
+    #blurring so we remove noises 
+    gray = cv2.GaussianBlur(gray, (21,21),0)
+    # we get the first frame
+    if first_frame is None:
+        first_frame = gray
+        continue
+    
+    #creating a new img based on the first frame and the new frames
+    comparing_frames = cv2.absdiff(first_frame,gray)
+        
+    cv2.imshow("Live", gray)
+    cv2.imshow("Mixed Frames", comparing_frames)
     key = cv2.waitKey(1)
     
     
